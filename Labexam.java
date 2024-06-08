5763906ef86758f30936c8b6988d1e083ee1c9c5
gere-77 <geremewderese21@gmail.com>
2024-06-08 17:33:26 +0300

    Add files via upload

---
+++ Labexam.java
@@ -0,0 +1,19 @@
+
+public class labexam1 {
+
+}
+public class StudentRepo {
+	 public void createDBAndTable() { /* implementation */ }
+	 public void insertIntoTable(Student student) { /* implementation */
+	}
+	}
+<?xml version="1.0" encoding="UTF-8"?>
+<beans xmlns="http://www.springframework.org/schema/beans"
+ xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+ xsi:schemaLocation="http://www.springframework.org/schema/beans
+ 
+http://www.springframework.org/schema/beans/spring-beans.xsd">
+</beans>
+public class Launcher {
+	 public static void main(String[] args) { /* implementation */ }
+	}
